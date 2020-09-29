a8c1542ea714f5a3b9563eb661e52868ebb7233c
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-09-29 10:48:19 -0600

    Primera versión

---
+++ README.md
@@ -0,0 +1,7 @@
+# CETis 108
+## Programación
+### Desarrolla aplicaciones web con acceso a base de datos
+
+Profesor: **Bidkar Aragón Cárdenas**
+
+Alumno: **?**
---
+++ src/public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bienvenido al desarrollo web</title>
+</head>
+<body>
+  <h1>
+    <?php echo 'Bienvenido al desarrollo web'; ?>
+  </h1>
+</body>
+</html>
